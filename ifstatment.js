1f3935fb2512f26dfe380ff27ad01e325788f619
Kaique Dos Santos <148874419+kaiquewill@users.noreply.github.com>
2023-12-18 00:58:23 +0000

    If Statment JS

---
+++ ifstatment.js
@@ -0,0 +1,24 @@
+
+/*
+let age = -4
+
+if(age>= 18){
+    console.log(" you are an adult");
+}
+else if(age <0)
+    console.log("you havent being born yet")
+
+else{
+    console.log(" you are a kid");
+}
+*/
+
+
+let online = false;
+
+if(online){
+    console.log("you are online ")
+}
+else{
+    console.log("you are offiline")
+}
