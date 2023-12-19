fd6e1e642e9fe024e01112659f3896bd079191e2
Kaique Dos Santos <148874419+kaiquewill@users.noreply.github.com>
2023-12-19 03:17:19 +0000

    Switch function

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let username;
-document.getElementById("mybutton").onclick=function(){
-    username = document.getElementById("mytext").value;
-    console.log(username);
-    document.getElementById("mylabel").innerHTML = "hello " + username;
+
+let grade = "B"; 
+
+switch(grade){
+    case "A":
+        console.log("you did great")
+        break;
+
+    case "B":
+         console.log("you did good")
+            break;
+    case "C":
+         console.log("you did ok")
+            break;
+
+    case "D":
+         console.log("you did shit")
+            break;
+    case "F":
+        console.log("you fail")
+            break;
+
 }
