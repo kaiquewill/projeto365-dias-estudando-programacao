1d65b50c9d4d0c1d0ab6da647f5b4cb3b7bd466e
Kaique Dos Santos <148874419+kaiquewill@users.noreply.github.com>
2023-12-23 16:07:40 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 
 
-let temp = 15;
-let sunny = true;
 
-if(!(temp > 0 )){
-    console.log("its cold outside")
-}
-else{
-    console.log("its warm outside")
-
+for (let i = 10; i > 0; i-=4 ){
+    console.log(i);
 }
 
-if(!sunny){
-    console.log("its sunny outside")
-}
-else{
-    console.log("its not sunny outside")
-
-}
-
+console.log("happy new year")
