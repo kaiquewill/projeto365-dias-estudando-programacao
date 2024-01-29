3b4b928bc6a495dd0751c44e2109a05c4f2c7d65
Kaique Dos Santos <148874419+kaiquewill@users.noreply.github.com>
2024-01-29 23:15:56 +0000

    Array

---
+++ array.js
@@ -0,0 +1,17 @@
+
+
+
+let fruit = ["apple","peach","grapes"];
+
+fruit.push("lemon");
+fruit.pop();
+fruit.unshift("mango")
+fruit.shift();
+
+
+let lenght = fruit.length;
+
+let index = fruit.indexOf("peach")
+
+
+console.log(index);
